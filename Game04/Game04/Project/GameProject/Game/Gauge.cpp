0d95bb03d07abe15f00f7352c8a4749d4aed74d1
Na26ki <on051213n@gmail.com>
2022-12-21 13:43:47 +0900

    g

--- Game04/Game04/Project/GameProject/Game/Gauge.cpp
+++ Game04/Game04/Project/GameProject/Game/Gauge.cpp
@@ -13,10 +13,6 @@ void Gauge::Draw() {
 	m_img.SetSize(256, 64);
 	m_img.SetPos(m_pos);
 	m_img.Draw();
-	//	m_img.SetRect(0, 0, 256, 64);
-	//	m_img.SetSize(256, 64);
-	//	m_img.SetPos(m_pos);
-	//	m_img.Draw();
 
 	//ÉQÅ[ÉWÇÃï\é¶
 	//ÉQÅ[ÉWÇÃí∑Ç≥ 4ÇÕògÇÃëæÇ≥
@@ -27,14 +23,6 @@ void Gauge::Draw() {
 	m_img.SetSize(width, 64 - border - border);
 	m_img.SetPos(m_pos + CVector2D(border, border));
 	m_img.Draw();
-	//	int border = 4;
-	//	int width = (256 - border - border) * m_par;
-	//	int y = (m_gauge_type + 1);
-	//	m_img.SetRect(border, (64 * y) + border, 4 + width, (64 * (y + 1)) - border);
-	//	m_img.SetSize(width, 64 - border - border);
-	//	m_img.SetPos(m_pos + CVector2D(border, border));
-	//	m_img.Draw();
-
 }
 void Gauge::SetValue(float par)
 {
